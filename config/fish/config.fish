8e71a6dcb0bc3ac4997cee1dea7d451f80340462
Nathan Smith <nathan@nfsmith.ca>
2019-06-29 18:02:39 -0700

    Cute banner

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,13 @@
+function memory_usage
+    free | grep Mem | awk '{printf "%0.1f%", (($2 - $7) / $2) * 100}'
+end
+
+set fish_greeting "Date: "(set_color purple)(date --rfc-3339=seconds)(set_color normal)"
+Host: "(set_color blue)(hostname)(set_color normal)"
+Path: "(set_color green)(pwd)(set_color normal)"
+RAM: "(set_color yellow)(memory_usage)(set_color normal)"
+CPU: "(set_color red)(echo unknown)(set_color normal)
+
 function fish_prompt
     set -l color_cwd
     set -l suffix
