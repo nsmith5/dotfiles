b9143ab267805fa5b9417e865eedf038cc2c605a
Nathan Smith <nathan@chainguard.dev>
2022-09-02 13:09:08 -0700

    "K" for golang function info in nvim

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -5,6 +5,9 @@ lspconfig.gopls.setup {
     cmd = {"gopls", "serve"},
     filetypes = {"go", "gomod"},
     root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+    on_attach = function()
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0 })
+    end,
     settings = {
         gopls = {
             analyses = {
