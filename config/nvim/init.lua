d5fdd260c18dc2933032694a1321ee8e9fbdfcaf
Nathan Smith <nathan@chainguard.dev>
2022-09-02 12:05:38 -0700

    vim -> neovim everywhere

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,1 +1,12 @@
 require('plugins')
+require('keyset')
+require('lsp')
+
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 10
+vim.opt.relativenumber = true
+
+-- color scheme
+vim.cmd('colorscheme base16-atelier-savanna-light')
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,7 +1,17 @@
 local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.config/nvim/plugged')
 
+-- Telescope is fuzzy finder magic
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
+Plug 'kyazdani42/nvim-web-devicons'
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+
+-- LSP (Language server protocol) setup
+Plug 'neovim/nvim-lspconfig'
+
+-- Random plugins
+Plug 'preservim/nerdtree'
+Plug 'RRethy/nvim-base16'
 
 vim.call('plug#end')
